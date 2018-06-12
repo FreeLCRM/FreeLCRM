91b6823220777e42060f7a79ca4c03fa87c5417f
Quentin Ganci <quentinganci@MacBook-Pro-de-Quentin.local>
2018-06-12 16:33:10 +0200

    Mise en place du formulaire dinscription

--- views/home_view.php
+++ views/home_view.php
@@ -8,8 +8,8 @@
     <div class="title">
         <h1>.freelcrm</h1>
     </div>
-    <div class="connexion-block">
-        <div class="input-connexion">
+    <div class="connexion-block" >
+        <div class="input-connexion" id="js-hide">
             <h1>Connexion</h1>
             <form action="#">
 
@@ -18,11 +18,33 @@
 
                 <label class="label-css-input" for="lname">Mot de passe</label>
                 <input class="input-css-text" type="password" name="lname"><br>
+
                 <a class="input-css-text-link" href="#">Mot de passe oublié ?</a>
                 <button class="btn-css-input" type="submit">Connexion</button>
             </form>
+
             <div class="bottom-form">
-                <a href="#">Nouveau ? Inscrivez-vous !</a>
+                <a href="#" onclick="myFunction()" >Nouveau ? Inscrivez-vous !</a>
+            </div>
+        </div>
+
+        <div class="input-connexion" id="js-show" style="display: none">
+            <h1>Inscription</h1>
+            <form action="#">
+                <label class="label-css-input" for="fname">Mail</label>
+                <input class="input-css-text" type="text" name="fname" placeholder="exemple@gmail.com"><br>
+
+                <label class="label-css-input" for="lname">Mot de passe</label>
+                <input class="input-css-text" type="password" name="lname"><br>
+
+                <label class="label-css-input" for="lname">Confirmer mot de passe</label>
+                <input class="input-css-text" type="password" name="lname"><br>
+
+                <button class="btn-css-input-signup" type="submit">Inscription</button>
+            </form>
+
+            <div class="bottom-form">
+                <a href="#" onclick="Show()" >Déjà membre ? Connectez-vous !</a>
             </div>
         </div>
     </div>
--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,3 +1,17 @@
 <footer>
     footer
 </footer>
+
+<script>
+    function myFunction() {
+        document.getElementById("js-hide").style.display = "none";
+        document.getElementById("js-show").style.display = "block";
+        console.log("Hi");
+    }
+    function Show() {
+        document.getElementById("js-show").style.display = "none";
+        document.getElementById("js-hide").style.display = "block";
+        console.log("no");
+    }
+</script>
+
