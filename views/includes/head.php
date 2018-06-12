a49f5d4b3bf977ae0465cca4e176b8aa44882ddd
Quentin Ganci <quentinganci@MacBook-Pro-de-Quentin.local>
2018-06-12 11:16:34 +0200

    Correction du lien dans le head et cration du fichier scss de la premiere page

--- views/includes/head.php
+++ views/includes/head.php
@@ -4,5 +4,5 @@
     <title><?= ucfirst($page)?> - FreelCRM</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/assets/styles/css/app.css">
+    <link rel="stylesheet" href="./assets/styles/css/app.css">
 <body>
