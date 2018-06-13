d09152a3c2d58bff0eb6362663e484ef86cfec48
Quentin Ganci <quentinganci@mbp-de-quentin.home>
2018-06-13 10:16:09 +0200

    Config page

---
+++ views/configuration.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<body>
+<?php include_once 'views/includes/head.php' ?>
+<?php include_once 'views/includes/header.php' ?>
+
+<div id="configuration-section">
+    <div class="container">
+        <h1>.freelcrm</h1>
+    </div>
+</div>
+
+<?php include_once 'views/includes/footer.php' ?>
+</body>
+</html>
+
